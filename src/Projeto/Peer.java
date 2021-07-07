f2622e7ba3872f97883fdbad248ec13432164b5a
Paulo Simoes <paulo.alexander@aluno.ufabc.edu.br>
2021-07-07 00:31:58 -0300

    Classes Servidor, Mensagem e Peer

--- src/Projeto/Peer.java
+++ src/Projeto/Peer.java
@@ -1,38 +1,95 @@
 package Projeto;
 
-public class Peer extends Thread
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class Peer
 {
-    private int id;
-    private String name;
-
-    public Peer()
+    public static class ClientPeer extends Thread
     {
+        private int id;
+        private String name;
 
-    }
-    public Peer(int id, String name)
-    {
-        this.id = id;
-        this.name = name;
-    }
-
-    public void run()
-    {
-        System.out.println("Entrei na thread");
-        int i =0;
-        while(i<4)
+        public ClientPeer(int id, String name)
         {
-            System.out.println("Thread:" + this.id + "momento:" + i);
-            i++;
+            this.id = id;
+            this.name = name;
+            this.start();
         }
-        try {
-            Thread.sleep(50);
-        }
-        catch (Exception ex)
+
+
+        public void run()
         {
-            System.out.println(ex.getMessage());
+            while (true)
+            {
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+                        System.in));
+
+                DatagramSocket clientSocket = null;
+                try {
+                    clientSocket = new DatagramSocket();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }
+
+                String servidor = "localhost";
+                int porta = 10098;
+
+                InetAddress IPAddress = null;
+                try {
+                    IPAddress = InetAddress.getByName(servidor);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+
+                byte[] sendData = new byte[1024];
+                byte[] receiveData = new byte[1024];
+
+                System.out.println("Digite o texto a ser enviado ao servidor: ");
+                String sentence = null;
+                try {
+                    sentence = inFromUser.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                sendData = sentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData,
+                        sendData.length, IPAddress, porta);
+
+                System.out
+                        .println("Enviando pacote UDP para " + servidor + ":" + porta);
+                try {
+                    clientSocket.send(sendPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                        receiveData.length);
+
+                try {
+                    clientSocket.receive(receivePacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Pacote UDP recebido...");
+
+                String modifiedSentence = new String(receivePacket.getData());
+
+                System.out.println("Texto recebido do servidor:" + modifiedSentence);
+                clientSocket.close();
+                System.out.println("Socket cliente fechado!");
+            }
+
         }
-
-
     }
 
+    
+    public static void main(String args[])
+    {
+        ClientPeer udpClient = new ClientPeer(1,"first");
+
+    }
 }
--- src/Projeto/Servidor.java
+++ src/Projeto/Servidor.java
@@ -1,13 +1,140 @@
 package Projeto;
 
+import java.io.IOException;
+import java.net.*;
 import java.util.ArrayList;
 
-public class Servidor
-{
-   String ip;
-   String porta;
-   ArrayList<Peer> lstPeer;
 
 
+public class Servidor {
 
+   private static class ServerPeer
+   {
+      Server central;
+      UDPServer UDPCon;
+
+      public ServerPeer(Server one) throws IOException
+      {
+         this.central = one;
+         this.UDPCon = new UDPServer(this);
+
+      }
+   }
+   private static class Server
+   {
+      public boolean on;
+      public int porta;
+      public ArrayList<ServerPeer> lstServerPeer;
+
+
+      public Server(int porta)
+      {
+         this.on = true;
+         this.porta = porta;
+         this.lstServerPeer = new ArrayList<ServerPeer>();
+      }
+      public int setNumConexion()
+      {
+         if (this.lstServerPeer.size() == 0)
+            return 1;
+         else
+            return this.lstServerPeer.size() + 1;
+      }
+
+   }
+
+   private static class UDPServer extends Thread
+   {
+      ServerPeer myPeer;
+      int numConn;
+
+      public UDPServer(ServerPeer peer)
+      {
+         this.myPeer = peer;
+         this.start();
+      }
+
+      public void run()
+      {
+         try
+         {
+            boolean conexion = this.UDPServerConexion();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+
+      public boolean UDPServerConexion() throws IOException
+      {
+         int porta = this.myPeer.central.porta;
+         DatagramSocket serverSocket = new DatagramSocket(porta);
+
+         byte[] receiveData = new byte[1024];
+         byte[] sendData = new byte[1024];
+
+         while (true)
+         {
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                    receiveData.length);
+            System.out.println("Esperando por datagrama UDP na porta " + porta);
+            try
+            {
+               serverSocket.receive(receivePacket);
+            }
+            catch (IOException e)
+            {
+               e.printStackTrace();
+            }
+            System.out.print("Datagrama UDP [" + numConn + "] recebido...");
+
+            String sentence = new String(receivePacket.getData());
+            System.out.println(sentence);
+
+            InetAddress IPAddress = receivePacket.getAddress();
+
+            int port = receivePacket.getPort();
+
+            String capitalizedSentence = sentence.toUpperCase();
+
+            sendData = capitalizedSentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+            System.out.print("Enviando " + capitalizedSentence + "...");
+
+            serverSocket.send(sendPacket);
+            System.out.println("OK\n");
+         }
+      }
+   }
+
+
+   public static void main(String[] args) throws Exception {
+      int porta = 10098;
+      Server server = new Server(porta);
+
+      System.out.println("Estou aqui");
+
+      ServerPeer current = null;
+
+      while (server.on) {
+         if (current == null) {
+            current = new ServerPeer(server);
+         }
+
+
+      }
+   }
 }
+
+
+
+
+
+
+
+
+
+
+
